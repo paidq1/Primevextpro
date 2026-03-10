b094318e07630b8c0158d66fc6a20a50cdbb7f1f
kelvinpaid <vertextradespro@gmail.com>
2026-03-11 00:42:30 +0530

    fix smartsupp widget position

--- src/components/Smartsupp.jsx
+++ src/components/Smartsupp.jsx
@@ -4,6 +4,9 @@ const Smartsupp = () => {
   useEffect(() => {
     var _smartsupp = window._smartsupp = window._smartsupp || {};
     _smartsupp.key = '63e738b95e6ce47a75abac7a15fb86fa9154ff24';
+    _smartsupp.alignX = 'right';
+    _smartsupp.offsetX = 10;
+    _smartsupp.offsetY = 10;
     window.smartsupp = window.smartsupp || function() { window.smartsupp._.push(arguments); };
     window.smartsupp._ = [];
     var s = document.getElementsByTagName('script')[0];
