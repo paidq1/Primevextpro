a971ba4e82d01cbd8b748612c824a327d9f2c6c7
paidq1 <primevextpro@gmail.com>
2026-03-20 22:38:35 +0530

    feat: notifications page with view all button

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,7 @@ import WithdrawVerifyCode from "./pages/WithdrawVerifyCode";
 import BotTransactionHistory from "./pages/BotTransactionHistory";
 import VerifyEmail from "./pages/VerifyEmail";
 import AdminPanel from "./pages/AdminPanel";
+import Notifications from "./pages/Notifications";
 import SupportPage from "./pages/SupportPage";
 import ResetPassword from "./pages/ResetPassword";
 import TradingInfo from "./pages/TradingInfo";
@@ -111,6 +112,7 @@ function App() {
         <Route path="/signin" element={<SignIn />} />
         <Route path="/verify-email/:token" element={<VerifyEmail />} />
         <Route path="/admin" element={<AdminRoute><AdminPanel /></AdminRoute>} />
+        <Route path="/dashboard/notifications" element={<PrivateRoute><Notifications /></PrivateRoute>} />
         <Route path="/admin/support" element={<AdminRoute><SupportPage /></AdminRoute>} />
         <Route path="/reset-password/:token" element={<ResetPassword />} />
         <Route path="/check-email" element={<CheckEmail />} />
